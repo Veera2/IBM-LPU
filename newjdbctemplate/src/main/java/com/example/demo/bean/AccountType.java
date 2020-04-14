4984ad80718ee7480c742a27a0097e68e667f89a
Lenovo <Lenovo@DESKTOP-1A5ADEC>
2020-04-14 05:46:09 +0530

    commit

--- newjdbctemplate/src/main/java/com/example/demo/bean/AccountType.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.bean;
-
-public enum AccountType {
-	SAVINGS,
-	CURRENT,
-	CREDIT_CARD,
-	LOAN
-
-}
--- newjdbctemplate/src/main/java/com/example/demo/service/ToDoService.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.bean.ToDo;
-
-public interface ToDoService {
-	public ToDo createToDo(ToDo todo);
-
-}
